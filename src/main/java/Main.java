d57fcf5b65ae8b68372929867e050ac9222b6f5e
Your Namescualy <you@example.compascal.guillen@gmail.com>
2017-02-08 09:10:07 +0100

    change dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,3 @@
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import spark.ModelAndView;
-import spark.template.freemarker.FreeMarkerEngine;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 import static spark.Spark.*;
 
 public class Main {
@@ -17,7 +5,6 @@ public class Main {
   public static void main(String[] args) {
 
     port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
 
     get("/", (req, res) -> "Hello World");
 	get("/test", (req, res) -> "Hello World test");
