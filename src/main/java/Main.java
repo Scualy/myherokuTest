1512d9fc26690b5b7e9f1c4990a77b4982bc22c1
Scualy <pascal.guillen@gmail.com>
2023-07-17 17:33:02 +0200

    test vscode.dev

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
     port(Integer.valueOf(System.getenv("PORT")));
     get("/", (req, res) -> "Hello World");
-	get("/test", (req, res) -> "Hello World test");
-  get("/test2", (req, res) -> "Hello World test");
+    get("/test", (req, res) -> "Hello World test");
+    get("/test2", (req, res) -> "Hello World test");
   }
 }
